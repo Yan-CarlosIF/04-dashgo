2a89c297f2b0c112f123b6a44382adcc92923b63
Yan Carlos <yan312005@gmail.com>
2025-06-27 16:24:33 -0300

    feat: new dashboard page and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Flex as="div" w="100%" h="100vh" justify="center" align="center">
+      <Flex
+        flexDir="column"
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+      >
+        <Stack spacing={4}>
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+        <Button type="submit" mt={6} size="lg" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
